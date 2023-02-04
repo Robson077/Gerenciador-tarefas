976a53e87bf8230531b244927510488ea802b97c
Robson077 <minhamae830@gmail.com>
2023-02-03 23:56:44 -0200

    feat: add task item component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// api: https://fsc-task-manager-backend.herokuapp.com/tasks
+import Tasks from "./components/Tasks"
+
+const App = () => {
+  
+
+  return (
+    <>
+      <Tasks />
+    </>
+  );
+}
+
+export default App
