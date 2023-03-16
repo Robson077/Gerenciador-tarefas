0e3c549f0ad49469533c0b246b49f5e4fe1ab0a7
Robson077 <minhamae830@gmail.com>
2023-03-16 13:20:30 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,15 +1,36 @@
 import { useState } from "react"
+import {FaPlus} from "react-icons/fa"
+import axios from "axios"
+import {useAlert} from "react-alert"
 
 import "./AddTask.scss"
 
 import CustomInput from "./CustomInput"
+import CustomButton from "./CustomButton"
 
 const AddTask = () => {
 
     const [task, setTask] = useState("")
 
+    const alert = useAlert()
+
     const onChange = (e) => {
         setTask(e.target.value)
+    }
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0) {
+                return alert.error("A tarefa precisa de uma descrição para ser adicionada.")
+            }
+
+            await axios.post("https://fsc-task-manager-backend.herokuapp.com/tasks", {
+                description: task,
+                isCompleted: false
+            })
+        } catch (error) {
+
+        }
     }
 
     return (
@@ -19,6 +40,10 @@ const AddTask = () => {
                 value={task} 
                 onChange={onChange}
             />
+
+            <CustomButton onClick={handleTaskAddition}>
+                <FaPlus size={14} color="#fff" />
+            </CustomButton>
         </div>
     )
 }
